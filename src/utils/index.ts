c2fea75d2ad5e9989de43cf26934963c80e5e068
Jay Chiarella <jay.gch93@gmail.com>
2024-03-24 07:11:39 +0100

    fix url thidr time

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
 export const BACKEND_URL =
   process.env.NODE_ENV === "production"
-    ? "http://railway-backend.railway.internal"
+    ? "http://railway-backend.railway.internal:3000" // hardcode it for now, don't expect it to change
     : "http://localhost:3000";
