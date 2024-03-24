3cad62bc017b65934085637d5dcb011abbda58c5
Jay Chiarella <jay.gch93@gmail.com>
2024-03-24 07:10:47 +0100

    again fix

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
 export const BACKEND_URL =
   process.env.NODE_ENV === "production"
-    ? "railway-backend.railway.internal"
+    ? "http://railway-backend.railway.internal"
     : "http://localhost:3000";
